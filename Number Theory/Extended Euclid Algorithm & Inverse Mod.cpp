3aa1eafa86c8c1b9982726687a98870dc7319b79
Sowrav14 <sowravnath05102002@gmail.com>
2024-09-03 11:41:23 +0600

    Number theory folder added

---
+++ Number Theory/Extended Euclid Algorithm & Inverse Mod.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define Fast_IO() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+const int N = 1e5 + 10;
+const int M = 1e9 + 7;
+const int inf = 1e12;
+
+
+int extended_euclid(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1; y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclid(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+int inverse(int a, int m) {
+    int x, y;
+    int g = extended_euclid(a, m, x, y);
+    if (g != 1) return -1;
+    return (x % m + m) % m;
+}
+
+
+void solve(){
+
+    int x = 100, m = 37;
+    cout << inverse(x, m) << endl;
+
+}
+
+signed main(){
+    Fast_IO()
+    int t = 1;
+    // cin >> t;
+    for(int i=1;i<=t;i++){
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+}
